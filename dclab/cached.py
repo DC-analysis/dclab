64cb952e1530d71a12a558c4f013ec1cbb51c45e
paulmueller <paul.mueller@biotec.tu-dresden.de>
2015-11-21 12:42:18 +0100

    implemented maximum cache size

--- dclab/cached.py
+++ dclab/cached.py
@@ -1,19 +1,31 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 """
-Cache for fast recomputation
+Cache for fast "recomputation"
 """
 from __future__ import division, print_function
 
 import hashlib
 import numpy as np
 
+
+MAX_SIZE = 100
+
 class Cache(object):
     """
     A cache that can be used to decorate methods that accept
     numpy ndarrays as arguments.
+    
+    - cache is based on dictionary
+    - md5 hashes of method arguments are the dictionary keys
+    - applicable decorator for all methods in a module
+    - applicable to methods with the same name in different source files
+    - set cache size with `cached.MAX_SIZE`
+    - only one global cache is generated, there are no instances of `Cache`
+
     """
     _cache = {}
+    _keys = []
     def __init__(self, func):
         self.func = func
     def __call__(self, *args, **kwargs):
@@ -48,5 +60,9 @@ class Cache(object):
         else:
             data = self.func(*args, **kwargs)
             Cache._cache[ref] = data
+            Cache._keys.append(ref)
+            if len(Cache._keys) > MAX_SIZE:
+                delref = Cache._keys.pop(0)
+                Cache._cache.pop(delref)
             return data
             

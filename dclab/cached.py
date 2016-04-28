fix KDE computation and improve hasher

dclab/cached.py
@@ -28,32 +28,28 @@ class Cache(object):
     _keys = []
     def __init__(self, func):
         self.func = func
+
+    
     def __call__(self, *args, **kwargs):
-        ahash = hashlib.md5()
+        self.ahash = hashlib.md5()
 
         # hash arguments
         for arg in args:
-            if isinstance(arg, np.ndarray):
-                ahash.update(arg.view(np.uint8))
-            else:
-                ahash.update(arg)
+            self._update_hash(arg)
         
         # hash keyword arguments
         kwds = list(kwargs.keys())
         kwds.sort()
         for k in kwds:
-            ahash.update(k)
-            arg = kwargs[k]
-            if isinstance(arg, np.ndarray):
-                ahash.update(arg.view(np.uint8))
-            else:
-                ahash.update(arg)            
-        
+            self.ahash.update(k)
+            self._update_hash(kwargs[k])
+
+
         # make sure we are caching for the correct method
-        ahash.update(self.func.func_name)   
-        ahash.update(self.func.func_code.co_filename)
+        self._update_hash(self.func.func_name)   
+        self._update_hash(self.func.func_code.co_filename)
         
-        ref = ahash.hexdigest()
+        ref = self.ahash.hexdigest()
 
         if ref in Cache._cache:
             return Cache._cache[ref]
@@ -65,4 +61,15 @@ class Cache(object):
                 delref = Cache._keys.pop(0)
                 Cache._cache.pop(delref)
             return data
-            
+    
+    def _update_hash(self, arg):
+        """ Takes an argument and updates the hash.
+        The argument can be an np.array, string, or list
+        of things that are convertable to strings.
+        """
+        if isinstance(arg, np.ndarray):
+            self.ahash.update(arg.view(np.uint8))
+        elif isinstance(arg, list):
+            [ self.ahash.update(str(a)) for a in arg ]
+        else:
+            self.ahash.update(arg)

dclab/kde_methods.py
@@ -75,9 +75,11 @@ def kde_multivariate(events_x, events_y, bw, xout=None, yout=None):
         xout = events_x
         yout = events_y
     
-    positions = np.vstack([xout.ravel(), yout.ravel()])
-    estimator_ly = KDEMultivariate(data=[xout,yout],var_type='cc',
-                                   bw=bw)
+    positions = np.vstack([xout.flatten(), yout.flatten()])
+    estimator_ly = KDEMultivariate(data=[events_x.flatten(),
+                                         events_y.flatten()],
+                                   var_type='cc', bw=bw)
+
     density = estimator_ly.pdf(positions)
     return density.reshape(xout.shape)
     

dclab/rtdc_dataset.py
@@ -560,6 +560,7 @@ class RTDC_DataSet(object):
         else:
             x = getattr(self, dfn.cfgmaprev[xax])
             y = getattr(self, dfn.cfgmaprev[yax])
+
         # evaluation
         xlin = np.arange(x.min(), x.max(), deltaarea)
         ylin = np.arange(y.min(), y.max(), deltacirc)
@@ -576,6 +577,7 @@ class RTDC_DataSet(object):
             raise ValueError("Unknown KDE estimator {}".format(kde_type))
         
         print("KDE contour {} time: ".format(kde_type), time.time()-a)
+
         return xmesh, ymesh, density
 
 
@@ -625,7 +627,7 @@ class RTDC_DataSet(object):
         else:
             posx = positions[0]
             posy = positions[1]
-        
+
         if kde_type == "gauss":
             density = kde_methods.kde_gauss(x, y,
                                             xout=posx, yout=posy)
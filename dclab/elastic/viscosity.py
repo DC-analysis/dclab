a736646a6c19468bb7ac868beb306d6eebd0e20f
paulmueller <paulmueller@users.noreply.github.com>
2017-03-24 12:06:57 +0100

    add viscosity computation for CellCarrier

---
+++ dclab/elastic/viscosity.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+RTDC_DataSet configuration
+"""
+from __future__ import division, print_function, unicode_literals
+
+
+def get_viscosity(medium="CellCarrier", channel_width=20.0, flow_rate=0.16,
+                  temperature=23.0):
+    """Returns the viscosity for RT-DC-specific media
+
+    
+    Parameters
+    ----------
+    medium: str
+        The medium to compute the viscosity for.
+        One of ["CellCarrier", "CellCarrierB"].
+    channel_width: float
+        The channel width in µm
+    flow_rate: float
+        Flow rate in µl/s
+    temperature: float or ndarray
+        Temperature in °C
+
+    Returns
+    -------
+    viscosity: float or ndarray
+        Viscosity in mPa*s
+
+    Notes
+    -----
+    The CellCarrier A and B media are optimized for RT-DC measurements.
+    """
+    assert medium in ["CellCarrier", "CellCarrierB"]
+    
+    # convert flow_rate from µl/s to m³/s
+    # convert channel_width from µm to m
+    term1 = 1.1856*6*flow_rate*1e-9/(channel_width*1e-6)**3 * 2/3
+    
+    if medium == "CellCarrier":
+        temp_corr = (temperature/23.2)**-.866
+        term2 = 0.6771/0.5928+0.2121/(0.5928*0.677)
+        eta = 0.179*(term1*term2)**(0.677-1)*temp_corr*1e3
+    elif medium == "CellCarrierB":
+        temp_corr = (temperature/23.6)**-.866
+        term2 = 0.6771/0.5928+0.2121/(0.5928*0.634)
+        eta = 0.360*(term1*term2)**(0.634-1)*temp_corr*1e3
+
+    return eta
+
---
+++ tests/test_elastic_viscosity.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" Downsampling only affects RTDC_DataSet._plot_filter
+"""
+from __future__ import print_function
+
+import codecs
+import numpy as np
+import os
+from os.path import abspath, dirname, join
+import shutil
+import sys
+import tempfile
+import warnings
+import zipfile
+
+
+# Add parent directory to beginning of path variable
+sys.path.insert(0, dirname(dirname(abspath(__file__))))
+from dclab.elastic import viscosity
+
+
+def test_cell_carrier():
+    """Test using table from Christophs script"""
+    ch_sizes = [15,15,15,20,20,20,20,20,30,30,30,40,40,40]
+    fl_rates = [0.016,0.032,0.048,0.02,0.04,0.06,0.08,0.12,0.16,0.24,0.32,0.32,0.40,0.60]
+    temps = [24,24,24,24,24,24,24,24,24,24,24,24,24,24]
+    eta_a = [5.8,4.6,4.1,7.1,5.7,5.0,4.5,4.0,5.4,4.7,4.3,5.7,5.3,4.6]
+    eta_b = [7.5,5.8,5.0,9.4,7.3,6.3,5.7,4.9,6.9,5.9,5.3,7.3,6.7,5.8]
+    
+    for c, f, t, a in zip(ch_sizes, fl_rates, temps, eta_a):
+        eta = viscosity.get_viscosity(medium="CellCarrier",
+                                       channel_width=c,
+                                       flow_rate=f,
+                                       temperature=t)
+        assert np.allclose(np.round(eta, 1), a)
+
+    for c, f, t, b in zip(ch_sizes, fl_rates, temps, eta_b):
+        eta = viscosity.get_viscosity(medium="CellCarrierB",
+                                       channel_width=c,
+                                       flow_rate=f,
+                                       temperature=t)
+        assert np.allclose(np.round(eta, 1), b)
+
+
+if __name__ == "__main__":
+    # Run all tests
+    loc = locals()
+    for key in list(loc.keys()):
+        if key.startswith("test_") and hasattr(loc[key], "__call__"):
+            loc[key]()
+    

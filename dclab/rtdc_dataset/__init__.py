9e77cadde7c59fdecae3e2bd3dc82961eb540f06
paulmueller <paulmueller@users.noreply.github.com>
2017-06-07 16:36:47 +0200

    add deprecation warning

--- dclab/rtdc_dataset/__init__.py
+++ dclab/rtdc_dataset/__init__.py
@@ -2,18 +2,51 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals 
 
-from .core import hashfile
+import sys
+import warnings
+
+from .core import hashfile, RTDCBase
 from .config import Configuration
 from . import fmt_dict, fmt_tdms, fmt_hierarchy
 
+if sys.version_info[0] == 2:
+    str_classes = (str, unicode)
+else:
+    str_classes = str
 
-def RTDC_DataSet(tdms_path=None, ddict=None, hparent=None):
-    kwinput = [tdms_path, ddict, hparent].count(None)
-    assert kwinput==2, "Specify tdms_path OR ddict OR hparent"
+
+def new_dataset(data):
+    """Initialize a new RT-DC data set
     
-    if tdms_path is not None:
-        return fmt_tdms.RTDC_TDMS(tdms_path)
-    elif ddict is not None:
-        return fmt_dict.RTDC_Dict(ddict)
-    elif hparent is not None:
-        return fmt_hierarchy.RTDC_Hierarchy(hparent)
+    Parameters
+    ----------
+    data:
+        can be one of the following:
+        - dict
+        - .tdms file
+        - subclass of `RTDCBase`
+    
+    Returns
+    -------
+    dataset: subclass of `RTDCBase`
+        A new dataset instance
+    """
+    if isinstance(data, dict):
+        return fmt_dict.RTDC_Dict(data)
+    elif isinstance(data, str_classes):
+        return fmt_tdms.RTDC_TDMS(data)
+    elif isinstance(data, RTDCBase):
+        return fmt_hierarchy.RTDC_Hierarchy(data)
+    else:
+        msg = "data type not supported: {}".format(data.__class__)
+        raise NotImplementedError(msg)
+
+
+def RTDC_DataSet(*args, **kwargs):
+    msg = "Please use `new_dataset` instead of `RTDC_DataSet` (DEPRECATION)!"
+    warnings.warn(msg)
+    if args:
+        return new_dataset(args[0])
+    else:
+        keys = list(kwargs.keys())
+        return new_dataset(kwargs[keys[0]])

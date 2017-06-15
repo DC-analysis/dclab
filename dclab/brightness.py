7008e3dece2b40faebbebf8e6b47b17a2880d8d0
Maik Herbig <maik-herbig@gmx.de>
2017-06-15 14:33:57 +0200

    Use Pillow to get the brightness
    Pillow also works for this purpose as an alternative to OpenCV. The function using Pillow is faster than the function using OpenCV (25 times faster for 4000 cells on my machine).

--- dclab/brightness.py
+++ dclab/brightness.py
@@ -6,10 +6,12 @@ contour for RT-DC measurements
 from __future__ import division, print_function, unicode_literals
 import numpy as np
 import cv2
+from PIL import Image, ImageDraw
 
 def get_brightness(images,cont):
     """
     Calculate the mean and std of the grayscale values inside the contour
+    using OpenCV
     
     Parameters
     ----------
@@ -55,6 +57,61 @@ def get_brightness(images,cont):
     if not type(cont)==list:
         # Do not return a list if the input contour was not in a list
         brightn_mean = brightn_mean[0]
+        brightn_std = brightn_std[0]
+     
+    return(brightn,brightn_std)
+
+def get_brightness_pillow(images,cont):
+    """
+    Calculate the mean and std of the grayscale values inside the contour
+    using Pillow
+    
+    Parameters
+    ----------
+    cont: ndarray or list of ndarrays of shape (N,2)
+        A 2D array that holds the contour of an event (in pixels)
+        e.g. obtained using `mm.contour` where  `mm` is an instance
+        of `RTDCBase`. The first and second columns of `cont`
+        correspond to the x- and y-coordinates of the contour.
+    images: ndarray or list of ndarrays 
+        A 2D array that holds the image in form of grayscale values of an event    
+
+    Returns
+    -------
+    brightn: float or ndarray
+        mean of the grayscale values in the contour
+    brightn_std: float or ndarray
+        standard deviation of the grayscale values in the contour
+    
+    """
+    if not type(cont)==list:
+        cont = [cont]
+    if not type(images)==list:
+        images = [images]
+    
+    msg = 'Nr. of images and contours is not equal'
+    assert len(images) == len(cont),msg
+    
+    #convert contours to PIL polygon format
+    cont = [a.ravel().tolist() for a in cont]
+
+    # results are stored in a separate array initialized with nans
+    brightn = np.zeros(len(images), dtype=float)*np.nan
+    brightn_std = np.zeros(len(images), dtype=float)*np.nan
+
+    for i in range(len(images)):
+        frameMask_i = Image.new('L', images[1].shape[::-1], 0)
+        ImageDraw.Draw(frameMask_i).polygon(cont[i], outline=1, fill=1)
+        frameMask_i = np.array(frameMask_i)
+        ind = np.where(frameMask_i==1)
+        Mean = np.mean(images[i][ind])           
+        Std = np.std(images[i][ind])                              
+        brightn[i] = Mean
+        brightn_std[i] = Std
+                   
+    if not type(cont)==list:
+        # Do not return a list if the input contour was not in a list
+        brightn = brightn[0]
         brightn_std = brightn_std[0]
      
     return(brightn,brightn_std)

7444f7d45fcd337765c3e784e4ad48ca01f153b8
paulmueller <paulmueller@users.noreply.github.com>
2017-09-27 11:05:09 +0200

    doc

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
     setup(
         name=name,
         author=author,
-        author_email='paul.mueller@biotec.tu-dresden.de',
+        author_email='dev@craban.de',
         url='https://github.com/ZELLMECHANIK-DRESDEN/dclab',
         version=version,
         packages=find_packages(),

eda42278c0cf0f9099229366c7c657b4ed90c0f4
paulmueller <paulmueller@users.noreply.github.com>
2018-02-01 23:05:55 +0100

    example cleanup

--- examples/isoelastics.py
+++ examples/isoelastics.py
@@ -6,29 +6,24 @@ figure 3 (lower left) of :cite:`Mokbel2017`.
 import matplotlib.pylab as plt
 from matplotlib import cm
 import numpy as np
+
 import dclab
 
-
-kwargs = {"col1": "area_um",
-          "col2": "deform",
-          "channel_width": 20,
-          "flow_rate": 0.04,
-          "viscosity": 15,
-          "add_px_err": False,
-          "px_um": 0.2}
+# parameters for isoelastics
+kwargs = {"col1": "area_um",  # x-axis
+          "col2": "deform",  # y-axis
+          "channel_width": 20,  # [um]
+          "flow_rate": 0.04,  # [ul/s]
+          "viscosity": 15,  # [Pa s]
+          "add_px_err": False  # no pixelation error
+          }
 
 analy = dclab.isoelastics.default.get(method="analytical", **kwargs)
 numer = dclab.isoelastics.default.get(method="numerical", **kwargs)
 
-# plot the properties of `qpi`
 plt.figure(figsize=(8, 4))
-
 ax1 = plt.subplot(111, title="elastic sphere isoelasticity lines")
-
-
 colors = [ cm.get_cmap("jet")(x) for x in np.linspace(0, 1, len(analy)) ]
-
-
 for aa, nn, cc in zip(analy, numer, colors):
     ax1.plot(aa[:, 0], aa[:, 1], color=cc)
     ax1.plot(nn[:, 0], nn[:, 1], color=cc, ls=":")

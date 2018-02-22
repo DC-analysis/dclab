fix: Young's modulus computation wrong due to faulty FEM simulations (#39)

tests/test_feat_emodulus.py
@@ -17,10 +17,17 @@ def test_simple_emod():
                                  medium="CellCarrier",
                                  channel_width=30,
                                  flow_rate=0.16,
-                                 px_um=0,  # withour pixelation correction
+                                 px_um=0,  # without pixelation correction
                                  temperature=23)
 
-    assert np.allclose(emod[10, 50], 0.93276932212481323)
+    assert np.allclose(emod[10, 50], 1.1875799054283109)
+    assert np.allclose(emod[50, 50], 0.55281291845478731)
+    assert np.allclose(emod[80, 50], 0.45678187644969814)
+
+    assert np.allclose(emod[10, 80], 1.5744560306483262)
+    assert np.allclose(emod[50, 80], 0.73534561544655519)
+    assert np.allclose(emod[80, 80], 0.60737083178222251)
+
 
 
 if __name__ == "__main__":
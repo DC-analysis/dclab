a8d665cde69e6656355a39d06f7047c8d81e6343
Paul Müller <dev@craban.de>
2018-06-09 12:06:30 +0200

    update setup dependencies

--- setup.py
+++ setup.py
@@ -44,10 +44,10 @@ setup(
     license="GPL v2",
     description=description,
     long_description=open('README.rst').read() if exists('README.rst') else '',
-    install_requires=["fcswrite",  # required by: fcs export
-                      "h5py",      # required by: hdf5 format
-                      "imageio",   # required by: tdms format, avi export
-                      "nptdms",    # required by: tdms format
+    install_requires=["fcswrite",        # required by: fcs export
+                      "h5py",            # required by: hdf5 format
+                      "imageio>=2.3.0",  # required by: tdms format, avi export
+                      "nptdms",          # required by: tdms format
                       "numpy>=1.5.1",
                       "pathlib",
                       "scipy>=0.12.0",

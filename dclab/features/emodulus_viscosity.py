a6c0340c3e89be1e44e7bdd9b548ff51e4641925
Paul Müller <dev@craban.de>
2018-10-10 15:55:30 +0200

    feat: add computation of viscosity for water (close #52)

--- dclab/features/emodulus_viscosity.py
+++ dclab/features/emodulus_viscosity.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-"""Viscosity computation for RT-DC media"""
+"""Viscosity computation for various media"""
 from __future__ import division, print_function, unicode_literals
+
+import numpy as np
 
 
 def get_viscosity(medium="CellCarrier", channel_width=20.0, flow_rate=0.16,
@@ -12,7 +14,7 @@ def get_viscosity(medium="CellCarrier", channel_width=20.0, flow_rate=0.16,
     ----------
     medium: str
         The medium to compute the viscosity for.
-        One of ["CellCarrier", "CellCarrier B"].
+        One of ["CellCarrier", "CellCarrier B", "water"].
     channel_width: float
         The channel width in µm
     flow_rate: float
@@ -27,23 +29,38 @@ def get_viscosity(medium="CellCarrier", channel_width=20.0, flow_rate=0.16,
 
     Notes
     -----
-    CellCarrier and CellCarrier B media are optimized for RT-DC measurements.
+    - CellCarrier and CellCarrier B media are optimized for
+      RT-DC measurements.
+    - Values for the viscosity of water are computed using
+      equation (15) from :cite:`Kestin_1978`.
     """
-    if medium.lower() not in ["cellcarrier", "cellcarrier b"]:
+    if medium.lower() not in ["cellcarrier", "cellcarrier b", "water"]:
         raise ValueError("Invalid medium: {}".format(medium))
-    
+
     # convert flow_rate from µl/s to m³/s
     # convert channel_width from µm to m
-    term1 = 1.1856*6*flow_rate*1e-9/(channel_width*1e-6)**3 * 2/3
-    
+    term1 = 1.1856 * 6 * flow_rate * 1e-9 / (channel_width * 1e-6)**3 * 2 / 3
+
     if medium == "CellCarrier":
-        temp_corr = (temperature/23.2)**-.866
-        term2 = 0.6771/0.5928+0.2121/(0.5928*0.677)
-        eta = 0.179*(term1*term2)**(0.677-1)*temp_corr*1e3
+        temp_corr = (temperature / 23.2)**-0.866
+        term2 = 0.6771 / 0.5928 + 0.2121 / (0.5928 * 0.677)
+        eta = 0.179 * (term1 * term2)**(0.677 - 1) * temp_corr * 1e3
     elif medium == "CellCarrier B":
-        temp_corr = (temperature/23.6)**-.866
-        term2 = 0.6771/0.5928+0.2121/(0.5928*0.634)
-        eta = 0.360*(term1*term2)**(0.634-1)*temp_corr*1e3
-
+        temp_corr = (temperature / 23.6)**-0.866
+        term2 = 0.6771 / 0.5928 + 0.2121 / (0.5928 * 0.634)
+        eta = 0.360 * (term1 * term2)**(0.634 - 1) * temp_corr * 1e3
+    elif medium == "water":
+        # see equation (15) in Kestin et al, J. Phys. Chem. 7(3) 1978
+        if np.min(temperature) < 0 or np.max(temperature) > 40:
+            msg = "For water, the temperature must be in [0, 40] degC! " \
+                  "Got min/max values of '{}'.".format(np.min(temperature),
+                                                       np.max(temperature))
+            raise ValueError(msg)
+        eta0 = 1.002  # [mPa]
+        right = (20-temperature) / (temperature + 96) \
+            * (+ 1.2364
+               - 1.37e-3 * (20 - temperature)
+               + 5.7e-6 * (20 - temperature)**2
+               )
+        eta = eta0 * 10**right
     return eta
-
--- tests/test_feat_emodulus_viscosity.py
+++ tests/test_feat_emodulus_viscosity.py
@@ -33,6 +33,50 @@ def test_cell_carrier():
         assert np.allclose(np.round(eta, 1), b)
 
 
+def test_water():
+    """Test with data from Kestin et al, J. Phys. Chem. 7(3) 1978"""
+    ref = np.array([[0, 1791.5],
+                    [5, 1519.3],
+                    [10, 1307.0],
+                    [15, 1138.3],
+                    [20, 1002.0],
+                    [25, 890.2],
+                    [30, 797.3],
+                    [35, 719.1],
+                    [40, 652.7],
+                    ])
+    ref[:, 1] *= 1e-3  # uPas to mPas
+
+    res = emodulus_viscosity.get_viscosity(medium="water",
+                                           temperature=ref[:, 0])
+    assert np.allclose(res, ref[:, 1], rtol=8e-5, atol=0)
+
+
+def test_water_range():
+    # test values
+    try:
+        emodulus_viscosity.get_viscosity(medium="water", temperature=-1)
+    except ValueError:
+        pass
+    else:
+        assert False, "temperature should be outside valid range"
+
+    try:
+        emodulus_viscosity.get_viscosity(medium="water", temperature=41)
+    except ValueError:
+        pass
+    else:
+        assert False, "temperature should be outside valid range"
+    # also test arrays
+    try:
+        emodulus_viscosity.get_viscosity(medium="water",
+                                         temperature=np.arange(-2, 10))
+    except ValueError:
+        pass
+    else:
+        assert False, "temperature should be outside valid range"
+
+
 if __name__ == "__main__":
     # Run all tests
     loc = locals()

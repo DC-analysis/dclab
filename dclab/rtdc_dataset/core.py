fix: range for contour KDE computation did not always contain end value

dclab/rtdc_dataset/core.py
@@ -326,8 +326,11 @@ class RTDCBase(object):
         xc = xs[~bad]
         yc = ys[~bad]
 
-        xlin = np.arange(xc.min(), xc.max(), xacc)
-        ylin = np.arange(yc.min(), yc.max(), yacc)
+        xnum = int(np.ceil((xc.max() - xc.min()) / xacc))
+        ynum = int(np.ceil((yc.max() - yc.min()) / yacc))
+
+        xlin = np.linspace(xc.min(), xc.max(), xnum, endpoint=True)
+        ylin = np.linspace(yc.min(), yc.max(), ynum, endpoint=True)
 
         xmesh, ymesh = np.meshgrid(xlin, ylin, indexing="ij")
 

tests/test_kde.py
@@ -68,7 +68,7 @@ def test_kde_log_scatter_points():
     a = ds.get_kde_scatter(yscale="log")
     b = ds.get_kde_scatter(yscale="log",
                            positions=[ds["area_um"], ds["deform"]])
-    
+
     assert np.all(a == b)
 
 
1c3c43b587edd986a4d4dac8d30d06179d88b219
Paul Müller <dev@craban.de>
2019-07-10 14:41:15 +0200

    tests: add test for manual filtering

--- tests/test_rtdc_filter.py
+++ tests/test_rtdc_filter.py
@@ -9,6 +9,16 @@ from dclab.rtdc_dataset import new_dataset
 from helper_methods import example_data_dict
 
 
+def test_filter_manual():
+    # make sure min/max values are filtered
+    ddict = example_data_dict(size=8472, keys=["area_um", "deform"])
+    ds = new_dataset(ddict)
+    ds.filter.manual[[0, 8471]] = False
+    ds.apply_filter()
+    assert len(ds["deform"][ds.filter.all]) == 8470
+    assert ds["deform"][1] == ds["deform"][ds.filter.all][0]
+
+
 def test_filter_min_max():
     # make sure min/max values are filtered
     ddict = example_data_dict(size=8472, keys=["area_um", "deform"])

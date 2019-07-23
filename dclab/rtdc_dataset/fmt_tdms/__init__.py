8974cbdbc1bcc384c313935f162806be7a3d16cd
Paul Müller <dev@craban.de>
2019-07-23 23:07:34 +0200

    revert check for image (not necessary)

--- dclab/rtdc_dataset/fmt_tdms/__init__.py
+++ dclab/rtdc_dataset/fmt_tdms/__init__.py
@@ -92,8 +92,7 @@ class RTDC_TDMS(RTDCBase):
         return self
 
     def __exit__(self, type, value, tb):
-        if "image" in self:
-            del self._events["image"]._image_data
+        del self._events["image"]._image_data
 
     def _init_data_with_tdms(self, tdms_filename):
         """Initializes the current RT-DC dataset with a tdms file.

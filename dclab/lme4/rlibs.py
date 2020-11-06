9df56bd408e0a804ce045de3264448c9d180485c
Paul Müller <dev@craban.de>
2020-11-06 22:38:54 +0100

    ref: find out why rpy2 does not work on macos

--- dclab/lme4/rlibs.py
+++ dclab/lme4/rlibs.py
@@ -30,10 +30,11 @@ for _lib in LIB_MIN_VERSIONS:
         _v_req = LIB_MIN_VERSIONS[_lib]
         if LooseVersion(_mods[_lib].__version__) < LooseVersion(_v_req):
             raise ValueError("Please install '{}>={}'!".format(_lib, _v_req))
-        # install submodules
+    except (ImportError, ValueError):
+        _mods[_lib] = MockPackage(_lib)
+    else:
+        # import submodules
         if _lib in LIB_SUBMODULES:
             for _sm in LIB_SUBMODULES[_lib]:
                 _mods[_sm] = importlib.import_module(_sm)
-    except (ImportError, ValueError):
-        _mods[_lib] = MockPackage(_lib)
     locals().update(_mods)
--- tests/test_lme4.py
+++ tests/test_lme4.py
@@ -1,11 +1,16 @@
+from distutils.version import LooseVersion
+
 import numpy as np
 
 import dclab
-from dclab.lme4 import Rlme4, rsetup
+from dclab.lme4 import Rlme4, rsetup, rlibs
 
 
 def test_import_rpy2():
+    import rpy2
     from rpy2 import situation
+    assert LooseVersion(rlibs.LIB_MIN_VERSIONS["rpy2"]) < LooseVersion(
+        rpy2.__version__)
     assert situation.get_r_home() is not None
 
 

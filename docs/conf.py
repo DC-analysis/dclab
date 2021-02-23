a7dbf30239a8dec8a2ce0fdf8f186141d81d38de
Paul Müller <dev@craban.de>
2021-02-23 10:10:12 +0100

    docs: fix author list in pdf

--- docs/conf.py
+++ docs/conf.py
@@ -177,7 +177,8 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
     (master_doc, 'dclab.tex', 'dclab Documentation',
-     author, 'manual'),
+     author.replace(', ', '\\and '),
+     'manual'),
 ]
 
 

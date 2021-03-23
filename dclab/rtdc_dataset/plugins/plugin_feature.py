24f3c5ee5d988a7a6a900f1229697fb3c70741b0
Eoghan O'Connell <eoclives@hotmail.com>
2021-03-23 14:01:41 +0100

    ref: added plugin_feature file

---
+++ dclab/rtdc_dataset/plugins/plugin_feature.py
@@ -0,0 +1,6 @@
+
+# Create PlugInFeature class which is child of AncillaryFeature
+# it should have methods that can be overridden by a script
+# The script should create a child class of PlugInFeature
+# The methods defined in the script class will override the
+# PlugInFeature class.

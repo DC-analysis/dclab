9928d8a5cdc5a11097c55e2f7ca32e68fb05d316
Eoghan O'Connell <eoclives@hotmail.com>
2021-04-13 13:35:43 +0200

    test: add test to show that shared methods will be populated when only one is called

--- tests/test_ancillaries.py
+++ tests/test_ancillaries.py
@@ -80,6 +80,24 @@ def test_af_time():
     assert np.all(np.diff(tt) > 0)
 
 
+def test_af_populate_both_brightness():
+    """ Calling ancillary features will automatically populate other features
+    that share methods. """
+    ds = dclab.new_dataset(retrieve_data("rtdc_data_traces_video_bright.zip"))
+    assert len(ds._ancillaries) == 1  # time is populated
+    # This is something low-level and should not be done in a script.
+    # Remove the brightness columns from RTDCBase to force computation with
+    # the image and contour columns.
+    _ = ds._events.pop("bright_avg")
+    _ = ds._events.pop("bright_sd")
+    _ = ds["bright_avg"]
+    # both "bright_avg" and "bright_sd" will be populated
+    assert len(ds._ancillaries) == 3
+    feats = ["time", "bright_sd", "bright_avg"]
+    for af_key in ds._ancillaries:
+        assert af_key in feats
+
+
 if __name__ == "__main__":
     # Run all tests
     loc = locals()

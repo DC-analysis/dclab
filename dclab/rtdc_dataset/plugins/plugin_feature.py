ad7a9d99777477699e670cf73d148e084e2b0a68
Eoghan O'Connell <eoclives@hotmail.com>
2021-04-27 18:38:54 +0200

    ref: added some feature removal functions

--- dclab/rtdc_dataset/plugins/plugin_feature.py
+++ dclab/rtdc_dataset/plugins/plugin_feature.py
@@ -1,9 +1,6 @@
-
-# Create PlugInFeature class which is child of AncillaryFeature
-# it should have methods that can be overridden by a script
-# The script should create a child class of PlugInFeature
-# The methods defined in the script class will override the
-# PlugInFeature class.
+"""
+.. versionadded:: 0.34.0
+"""
 
 import pathlib
 import importlib
@@ -12,19 +9,24 @@ import sys
 from ..ancillaries import AncillaryFeature
 
 
+_registered_plugin_instances = []
+
+
 def create_new_plugin_feature(plugin_path):
     """find an instanciate a PlugInFeature from a user-defined script"""
     info = find_plugin_feature_script(plugin_path)
-
+    plugin_list = []
     for ii in range(len(info["feature names"])):
         ref_info = {
             "feature_name": info["feature names"][ii],
             "method": info["method"],
-            "req_config": info["config required"],
-            "req_features": info["features required"],
+            "req_config": info["config required"][ii],
+            "req_features": info["features required"][ii],
             "priority": info["priority"],
         }
-        PlugInFeature(**ref_info)
+        plugin_list.append(PlugInFeature(**ref_info))
+        # add feature label etc
+    return plugin_list
 
 
 def find_plugin_feature_script(plugin_path):
@@ -36,6 +38,20 @@ def find_plugin_feature_script(plugin_path):
     # undo our path insertion
     sys.path.pop(0)
     return plugin.info
+
+
+def remove_plugin_feature(plugin_instance):
+    """Convenience function for removing a plugin instance"""
+    PlugInFeature.features.remove(plugin_instance)
+    PlugInFeature.feature_names.remove(plugin_instance.feature_name)
+
+
+def remove_all_plugin_features():
+    # I guess because we are built on AncillaryFeature, we don't need something
+    # like temp feats' _registered_temporary_features = []?
+    # I have done this below anyway...
+    for plugin in _registered_plugin_instances:
+        remove_plugin_feature(plugin)
 
 
 class PlugInFeature(AncillaryFeature):

4025ebba3763220e3b49403fc6e63529d443f35e
Eoghan O'Connell <eoclives@hotmail.com>
2021-05-06 16:18:14 +0200

    test: add label information and check for dfn feature information

--- tests/test_plugin_features.py
+++ tests/test_plugin_features.py
@@ -26,6 +26,17 @@ def get_plugin_file(plugin_name="plugin_test_example.py"):
     return plugin_path
 
 
+def compute_single_plugin_feature(rtdc_ds):
+    circ_per_area = rtdc_ds["circ"] / rtdc_ds["area_um"]
+    return circ_per_area
+
+
+def compute_multiple_plugin_features(rtdc_ds):
+    circ_per_area = rtdc_ds["circ"] / rtdc_ds["area_um"]
+    circ_times_area = rtdc_ds["circ"] * rtdc_ds["area_um"]
+    return {"circ_per_area": circ_per_area, "circ_times_area": circ_times_area}
+
+
 def test_create_plugin():
     plugin_path = get_plugin_file()
     plugin_list = dclab.load_plugin_feature(plugin_path)
@@ -48,39 +59,41 @@ def test_remove_all_plugin_features():
     ds = dclab.new_dataset(retrieve_data("rtdc_data_hdf5_rtfdc.zip"))
     assert "circ_per_area" in ds
     assert "circ_times_area" in ds
+    assert dclab.dfn.feature_exists("circ_per_area")
+    assert dclab.dfn.feature_exists("circ_times_area")
 
     remove_all_plugin_features()
 
     assert "circ_per_area" not in ds
     assert "circ_times_area" not in ds
+    assert not dclab.dfn.feature_exists("circ_per_area")
+    assert not dclab.dfn.feature_exists("circ_times_area")
 
 
-def test_plugin_metadata():
+def test_plugin_attributes():
     plugin_path = get_plugin_file()
     plugin_list = dclab.load_plugin_feature(plugin_path)
     pf1, pf2 = plugin_list
 
     plugin_file_info = find_plugin_feature_script(plugin_path)
 
-    assert pf1.plugin_metadata == plugin_file_info
-    assert pf2.plugin_metadata == plugin_file_info
-
-
-def compute_single_plugin_feature(rtdc_ds):
-    circ_per_area = rtdc_ds["circ"] / rtdc_ds["area_um"]
-    return circ_per_area
-
-
-def compute_multiple_plugin_features(rtdc_ds):
-    circ_per_area = rtdc_ds["circ"] / rtdc_ds["area_um"]
-    circ_times_area = rtdc_ds["circ"] * rtdc_ds["area_um"]
-    return {"circ_per_area": circ_per_area, "circ_times_area": circ_times_area}
+    assert pf1.plugin_path == plugin_path
+    assert pf2.plugin_path == plugin_path
+    assert pf1.plugin_info == plugin_file_info
+    assert pf2.plugin_info == plugin_file_info
+    assert pf1.feature_label == plugin_file_info["feature labels"][0]
+    assert pf2.feature_label == plugin_file_info["feature labels"][1]
+    assert pf1.is_scalar
+    assert pf2.is_scalar
 
 
 def test_single_plugin_feature():
     ds = dclab.new_dataset(retrieve_data("rtdc_data_hdf5_rtfdc.zip"))
 
+    plugin_path = ''
     other_info = {}
+    feature_label = "Circularity per Area"
+    is_scalar = True
     feature_info = {
         "feature_name": "circ_per_area",
         "method": compute_single_plugin_feature,
@@ -89,7 +102,8 @@ def test_single_plugin_feature():
         "req_func": lambda x: True,
         "priority": 1,
     }
-    _ = PlugInFeature(other_info, **feature_info)
+    _ = PlugInFeature(feature_label, is_scalar,
+                      plugin_path, other_info, **feature_info)
     assert "circ_per_area" in ds
 
     circ_per_area = ds["circ_per_area"]
@@ -104,10 +118,13 @@ def test_single_plugin_feature():
 def test_multiple_plugin_features():
     ds = dclab.new_dataset(retrieve_data("rtdc_data_hdf5_rtfdc.zip"))
 
+    plugin_path = ''
     other_info = {}
     feature_names = ["circ_per_area", "circ_times_area"]
+    feature_labels = ["Circularity per Area", "Circularity times Area"]
+    is_scalar = True
     plugin_list = []
-    for feature_name in feature_names:
+    for feature_name, feature_label in zip(feature_names, feature_labels):
         feature_info = {
             "feature_name": feature_name,
             "method": compute_multiple_plugin_features,
@@ -116,7 +133,8 @@ def test_multiple_plugin_features():
             "req_func": lambda x: True,
             "priority": 1,
         }
-        plugin_list.append(PlugInFeature(other_info, **feature_info))
+        plugin_list.append(PlugInFeature(
+            feature_label, is_scalar, plugin_path, other_info, **feature_info))
 
     assert "circ_per_area" in ds
     assert "circ_times_area" in ds

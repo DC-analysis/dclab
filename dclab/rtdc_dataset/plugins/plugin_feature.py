054488de710d884b9301385ae8745ca4c17a81ee
Eoghan O'Connell <eoclives@hotmail.com>
2021-05-07 15:38:31 +0200

    ref: reorder removal of plugin instance information and reorder attributes

--- dclab/rtdc_dataset/plugins/plugin_feature.py
+++ dclab/rtdc_dataset/plugins/plugin_feature.py
@@ -55,11 +55,12 @@ def import_plugin_feature_script(plugin_path):
 def remove_plugin_feature(plugin_instance):
     """Convenience function for removing a plugin instance"""
     if isinstance(plugin_instance, PlugInFeature):
+        if plugin_instance.feature_name in PlugInFeature.feature_names:
+            PlugInFeature.feature_names.remove(plugin_instance.feature_name)
+            dfn.remove_dfn_feature_info(
+                plugin_instance.feature_name,
+                plugin_instance.feature_label)
         PlugInFeature.features.remove(plugin_instance)
-        PlugInFeature.feature_names.remove(plugin_instance.feature_name)
-        dfn.remove_dfn_feature_info(
-            plugin_instance.feature_name,
-            plugin_instance.feature_label)
     else:
         raise TypeError(f"Type {type(plugin_instance)} should be an instance "
                         "of PlugInFeature.")
@@ -79,18 +80,24 @@ class PlugInFeature(AncillaryFeature):
         their own features. See the dclab repo examples/plugins folder for
         example plugins.
         """
-        super().__init__(**kwargs)
         self.plugin_path = plugin_path
         self.plugin_info = info
         self.feature_label = feature_label
         self.is_scalar = is_scalar
-        self._update_feature_name_and_label()
+        self._update_feature_name_and_label(
+            feature_name=kwargs["feature_name"])
+        super().__init__(**kwargs)
 
-    def _update_feature_name_and_label(self):
+    def _update_feature_name_and_label(self, feature_name):
         """Wrapper on the `dclab.dfn.update_dfn_with_feature_info` function for
         handling cases when `feature_label=""`
+
+        This should be excecuted before initializing the super class
+        (AncillaryFeature). If we don't do this, then `remove_plugin_feature`
+        may end up removing innate features e.g., "deform".
+
         """
         if self.feature_label == "":
             self.feature_label = None
         self.feature_label = dfn.update_dfn_with_feature_info(
-            self.feature_name, self.feature_label, self.is_scalar)
+            feature_name, self.feature_label, self.is_scalar)

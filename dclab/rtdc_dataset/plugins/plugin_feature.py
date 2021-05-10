9d8cab6524e53da2136963345e819e5db5211446
Eoghan O'Connell <eoclives@hotmail.com>
2021-05-10 14:55:14 +0200

    ref: put all parsing and functionality into PlugInFeature methods

--- dclab/rtdc_dataset/plugins/plugin_feature.py
+++ dclab/rtdc_dataset/plugins/plugin_feature.py
@@ -17,21 +17,13 @@ class PluginImportError(BaseException):
 def load_plugin_feature(plugin_path):
     """find an instanciate a PlugInFeature from a user-defined script"""
     info = import_plugin_feature_script(plugin_path)
+    if not isinstance(info["feature names"], list):
+        raise ValueError(
+            "'feature names' must be a list of strings.")
+
     plugin_list = []
-    for ii in range(len(info["feature names"])):
-        ancill_info = {
-            "feature_name": info["feature names"][ii],
-            "method": info["method"],
-            "req_config": info["config required"],
-            "req_features": info["features required"],
-            "req_func": info["method check required"],
-            "priority": info["priority"],
-        }
-        feature_label = info["feature labels"][ii]
-        is_scalar = info["scalar feature"]
-        plugin_list.append(PlugInFeature(
-            feature_label, is_scalar, plugin_path, info, **ancill_info))
-        # add feature label etc
+    for feature_name in info["feature names"]:
+        plugin_list.append(PlugInFeature(feature_name, info, plugin_path))
     return plugin_list
 
 
@@ -55,6 +47,9 @@ def import_plugin_feature_script(plugin_path):
 def remove_plugin_feature(plugin_instance):
     """Convenience function for removing a plugin instance"""
     if isinstance(plugin_instance, PlugInFeature):
+        # This check is necessary for situations where the PlugInFeature fails
+        # between updating the `dclab.dfn` file and initialising the
+        # AncillaryFeature
         if plugin_instance.feature_name in PlugInFeature.feature_names:
             PlugInFeature.feature_names.remove(plugin_instance.feature_name)
             dfn.remove_dfn_feature_info(
@@ -74,21 +69,55 @@ def remove_all_plugin_features():
 
 
 class PlugInFeature(AncillaryFeature):
-    def __init__(self, feature_label, is_scalar,
-                 plugin_path, info, **kwargs):
+    def __init__(self, feature_name, info, plugin_path=None):
         """Child class of `AncillaryFeature` which allows a user to create
         their own features. See the dclab repo examples/plugins folder for
         example plugins.
         """
+        self._plugin_feature_name = feature_name
+        self._plugin_original_info = info
+        self.feature_label = None
+        self.is_scalar = True
         self.plugin_path = plugin_path
-        self.plugin_info = info
-        self.feature_label = feature_label
-        self.is_scalar = is_scalar
-        self._update_feature_name_and_label(
-            feature_name=kwargs["feature_name"])
-        super().__init__(**kwargs)
+        self.plugin_feature_info = self._handle_plugin_info()
+        self._handle_ancill_info()
+        super().__init__(**self._ancill_info)
 
-    def _update_feature_name_and_label(self, feature_name):
+    def _handle_plugin_info(self):
+        """Grab the relevant plugin feature from `info` and then set the
+        default `info` values if necessary.
+        """
+        self._error_check_plugin_original_info()
+        self._update_feature_name_and_label()
+        plugin_feature_info = {
+            "method": self._plugin_original_info["method"],
+            "description": self._plugin_original_info.get(
+                "description", "Description of my feature"),
+            "long description": self._plugin_original_info.get(
+                "long description", "Long description of my feature"),
+            "feature name": self._plugin_feature_name,
+            "feature label": self.feature_label,
+            "features required": self._plugin_original_info.get(
+                "features required", []),
+            "config required": self._plugin_original_info.get(
+                "config required", []),
+            "method check required": self._plugin_original_info.get(
+                "method check required", lambda x: True),
+            "scalar feature": self.is_scalar,
+            "version": self._plugin_original_info.get("version", "unknown"),
+        }
+        return plugin_feature_info
+
+    def _handle_ancill_info(self):
+        self._ancill_info = {
+            "feature_name": self.plugin_feature_info["feature name"],
+            "method": self.plugin_feature_info["method"],
+            "req_config": self.plugin_feature_info["config required"],
+            "req_features": self.plugin_feature_info["features required"],
+            "req_func": self.plugin_feature_info["method check required"],
+        }
+
+    def _update_feature_name_and_label(self):
         """Wrapper on the `dclab.dfn.update_dfn_with_feature_info` function for
         handling cases when `feature_label=""`
 
@@ -97,7 +126,40 @@ class PlugInFeature(AncillaryFeature):
         may end up removing innate features e.g., "deform".
 
         """
+
+        idx = self._plugin_original_info["feature names"].index(
+            self._plugin_feature_name)
+        self.is_scalar = self._plugin_original_info["scalar feature"][idx]
+        self.feature_label = self._plugin_original_info["feature labels"][idx]
         if self.feature_label == "":
             self.feature_label = None
-        self.feature_label = dfn.update_dfn_with_feature_info(
-            feature_name, self.feature_label, self.is_scalar)
+        dfn.update_dfn_with_feature_info(
+            self._plugin_feature_name, self.feature_label, self.is_scalar)
+        if self.feature_label is None:
+            self.feature_label = dfn.feature_name2label[
+                self._plugin_feature_name]
+
+    def _error_check_plugin_original_info(self):
+        if not isinstance(self._plugin_original_info, dict):
+            raise ValueError(
+                "PlugInFeature input for `info` must be a dict, instead a "
+                f"'{type(self._plugin_original_info)}' was given.")
+
+        if self._plugin_feature_name not in \
+                self._plugin_original_info["feature names"]:
+            raise ValueError(
+                "PlugInFeature input for `feature_name` was not found in the "
+                "input `info` dict. `feature_name` = '{}', "
+                "`info['feature names']` = '{}'. ".format(
+                    self._plugin_feature_name,
+                    self._plugin_original_info['feature names']))
+
+        if "feature labels" not in self._plugin_original_info:
+            raise ValueError(
+                "'feature labels' was not found in the input `info` dict. ")
+
+        if not callable(self._plugin_original_info["method"]):
+            raise ValueError(
+                "The `method` you have provided in the input `info` is not "
+                f"callable ('{self._plugin_original_info['method']}' is not "
+                "a function).")

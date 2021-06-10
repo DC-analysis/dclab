5182678a40d9b4c1a18cd75f4bb0983660a49d29
Paul Müller <dev@craban.de>
2021-06-10 23:48:59 +0200

    docs: fix CLI section

--- dclab/cli/__init__.py
+++ dclab/cli/__init__.py
@@ -1,9 +1,9 @@
 # flake8: noqa: F401
 """command line interface"""
-from .task_compress import compress
-from .task_condense import condense
-from .task_join import join
-from .task_repack import repack
-from .task_split import split
-from .task_tdms2rtdc import tdms2rtdc
-from .task_verify_dataset import verify_dataset
+from .task_compress import compress, compress_parser
+from .task_condense import condense, condense_parser
+from .task_join import join, join_parser
+from .task_repack import repack, repack_parser
+from .task_split import split, split_parser
+from .task_tdms2rtdc import tdms2rtdc, tdms2rtdc_parser
+from .task_verify_dataset import verify_dataset, verify_dataset_parser

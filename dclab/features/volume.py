40d7d1faab6fff5c3b6c068d1e2bbe76c3180000
Paul Müller <dev@craban.de>
2021-10-22 10:30:53 +0200

    ref: Put copyright in doc string for vol_revolve

--- dclab/features/volume.py
+++ dclab/features/volume.py
@@ -143,6 +143,32 @@ def vol_revolve(r, z, point_scale=1):
     Implementation of the volRevolve function (2012-05-03) by Geoff Olynyk
     https://de.mathworks.com/matlabcentral/fileexchange/36525-volrevolve
 
+    Copyright (c) 2012, Geoff Olynyk
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are
+    met:
+
+    * Redistributions of source code must retain the above copyright notice,
+      this list of conditions and the following disclaimer.
+
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+    "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+    TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+    PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+    OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+    SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+    TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+    OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+    OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+    NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
     Parameters
     ----------
     r: 1d np.ndarray
@@ -169,32 +195,6 @@ def vol_revolve(r, z, point_scale=1):
       Chapman & Hall / CRC Press, 2002, e-ISBN 978-1-4200-3544-5.
       See Chapter 5, Section 5.4, doi: 10.1201/9781420035445.ch5
     """
-    # Copyright (c) 2012, Geoff Olynyk
-    # All rights reserved.
-    #
-    # Redistribution and use in source and binary forms, with or without
-    # modification, are permitted provided that the following conditions are
-    # met:
-    #
-    # * Redistributions of source code must retain the above copyright notice,
-    #   this list of conditions and the following disclaimer.
-    #
-    # * Redistributions in binary form must reproduce the above copyright
-    #   notice, this list of conditions and the following disclaimer in the
-    #   documentation and/or other materials provided with the distribution
-    #
-    # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-    # "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
-    # TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
-    # PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-    # OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-    # SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
-    # TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
-    # OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
-    # OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-    # NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-    # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
     # sanity checks
     assert len(r) == len(z)
     assert len(r) >= 3

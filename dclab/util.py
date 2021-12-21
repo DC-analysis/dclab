f1c66599925854a243aa30908c49b1a510d7d12f
Paul Müller <dev@craban.de>
2021-12-21 12:11:34 +0100

    enh: cache computed file hashes with additional os.stat info

--- dclab/util.py
+++ dclab/util.py
@@ -1,4 +1,5 @@
 """Utility methods"""
+import functools
 import hashlib
 import pathlib
 import warnings
@@ -29,9 +30,43 @@ def hashfile(fname, blocksize=65536, count=0, constructor=hashlib.md5,
         warnings.warn("The `hasher_class` argument is deprecated, please use "
                       "`constructor` instead.")
         constructor = hasher_class
+    path = pathlib.Path(fname).resolve()
+    return _hashfile_cached(
+        path=path,
+        stat=path.stat(),
+        blocksize=blocksize,
+        count=count,
+        constructor=constructor
+    )
+
+
+@functools.lru_cache(maxsize=100)
+def _hashfile_cached(path, stat, blocksize=65536, count=0,
+                     constructor=hashlib.md5):
+    """Cached hashfile using stat tuple as cache
+
+    This is a privat function. Please use `hashfile` instead!
+
+    Parameters
+    ----------
+    path: pathlib.Path
+        path to the file to be hashed
+    stat: named tuple
+        tuple of `os.stat_result` for `path`. This must be specified,
+        so that caching of the result is done properly in case the user
+        modified `path` (this function is wrapped with
+        functools.lru_cache)
+    blocksize: int
+        block size in bytes read from the file
+        (set to `0` to hash the entire file)
+    count: int
+        number of blocks read from the file
+    constructor: callable
+        hash algorithm constructor
+    """
+    assert stat, "We need stat for validating the cache"
     hasher = constructor()
-    fname = pathlib.Path(fname)
-    with fname.open('rb') as fd:
+    with path.open('rb') as fd:
         buf = fd.read(blocksize)
         ii = 0
         while len(buf) > 0:
---
+++ tests/test_util.py
@@ -0,0 +1,45 @@
+import pathlib
+import tempfile
+
+from dclab import util
+import pytest
+
+
+@pytest.fixture(autouse=True)
+def clear_cache():
+    util._hashfile_cached.cache_clear()
+
+
+def test_hashfile_basic():
+    td = pathlib.Path(tempfile.mkdtemp("test_hashfile"))
+    p1 = td / "test_1.txt"
+    p1.write_text("Lorem ipsum")
+    util.hashfile(p1)
+    util.hashfile(p1)
+    assert util._hashfile_cached.cache_info().misses == 1
+    assert util._hashfile_cached.cache_info().hits == 1
+    assert util._hashfile_cached.cache_info().currsize == 1
+
+    p2 = td / "test_2.txt"
+    p2.write_text("dolor sit amet.")
+    util.hashfile(p2)
+    assert util._hashfile_cached.cache_info().misses == 2
+    assert util._hashfile_cached.cache_info().hits == 1
+    assert util._hashfile_cached.cache_info().currsize == 2
+
+
+def test_hashfile_modified():
+    td = pathlib.Path(tempfile.mkdtemp("test_hashfile"))
+    p1 = td / "test_1.txt"
+    p1.write_text("Lorem ipsum")
+    util.hashfile(p1)
+    util.hashfile(p1)
+    assert util._hashfile_cached.cache_info().misses == 1
+    assert util._hashfile_cached.cache_info().hits == 1
+    assert util._hashfile_cached.cache_info().currsize == 1
+
+    p1.write_text("dolor sit amet.")
+    util.hashfile(p1)
+    assert util._hashfile_cached.cache_info().misses == 2
+    assert util._hashfile_cached.cache_info().hits == 1
+    assert util._hashfile_cached.cache_info().currsize == 2

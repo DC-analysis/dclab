a8c222546cd226d6bd3dde93dc87e72ba4accb5c
Paul Müller <dev@craban.de>
2022-05-03 20:31:53 +0200

    ci: installing all R dependencies to test everything is not required

--- dclab/lme4/rsetup.py
+++ dclab/lme4/rsetup.py
@@ -170,8 +170,10 @@ def install_lme4():
         utils.chooseCRANmirror(ind=1)
         # install lme4 to user data directory (say yes to user dir install)
         with AutoRConsole() as rc:
-            # Install with dependencies
-            utils.install_packages("lme4", dependencies=True)
+            # install statmod and nloptr first
+            # (Doesn't R have package dependencies?!)
+            utils.install_packages(
+                rpy2.robjects.vectors.StrVector(["statmod", "nloptr", "lme4"]))
         return rc
 
 

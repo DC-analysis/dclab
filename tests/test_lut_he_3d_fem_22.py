4757c0682c56d213091ee6877a19518a0b062ff0
Paul Müller <dev@craban.de>
2022-10-05 15:11:22 +0200

    fix convex contour (#188)

--- tests/test_lut_he_3d_fem_22.py
+++ tests/test_lut_he_3d_fem_22.py
@@ -6,7 +6,7 @@ from dclab.features import emodulus
 
 @pytest.mark.filterwarnings('ignore::dclab.features.emodulus.'
                             + 'YoungsModulusLookupTableExceededWarning')
-def test_simple_emod_he_2d_fem_22():
+def test_simple_emod_he_3d_fem_22():
     x = np.linspace(0, 250, 100)
     y = np.linspace(0, 0.1, 100)
     x, y = np.meshgrid(x, y)
@@ -16,14 +16,14 @@ def test_simple_emod_he_2d_fem_22():
                                  medium="CellCarrier",
                                  channel_width=30,
                                  flow_rate=0.16,
-                                 lut_data="HE-2D-FEM-22",
+                                 lut_data="HE-3D-FEM-22",
                                  px_um=0,  # without pixelation correction
                                  temperature=23)
 
-    assert np.allclose(emod[10, 50], 1.2520069112713232)
-    assert np.allclose(emod[50, 50], 0.6442165714687615)
-    assert np.allclose(emod[80, 50], 0.575373042660183)
+    assert np.allclose(emod[10, 50], 1.301024899951213)
+    assert np.allclose(emod[50, 50], 0.6713689164185259)
+    assert np.isnan(emod[80, 50])
 
-    assert np.allclose(emod[10, 80], 1.638297273096505)
-    assert np.allclose(emod[50, 80], 0.8191788744746755)
-    assert np.allclose(emod[80, 80], 0.716695164528623)
+    assert np.allclose(emod[10, 80], 1.7172087155883002)
+    assert np.allclose(emod[50, 80], 0.8595551172406175)
+    assert np.isnan(emod[80, 80])

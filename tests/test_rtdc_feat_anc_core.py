fix: implement cleaner solution for time computation (close #207)

tests/test_rtdc_feat_anc_core.py
@@ -204,10 +204,12 @@ def test_af_recomputed_on_hash_change(fake_af_cleanup_fixture, path):
 def test_af_time():
     pytest.importorskip("nptdms")
     ds = dclab.new_dataset(retrieve_data("fmt-tdms_minimal_2016.zip"))
-    tt = ds["time"]
-    assert tt[0] == 0
-    assert np.allclose(tt[1], 0.0385)
-    assert np.all(np.diff(tt) > 0)
+    time = ds["time"]
+    frame = ds["frame"]
+    fr = ds.config["imaging"]["frame rate"]
+    assert np.allclose(time[0], frame[0] / fr, atol=0, rtol=1e-7)
+    assert np.allclose(time[1], 82.526, atol=0, rtol=1e-7)
+    assert np.all(np.diff(time) > 0)
 
 
 @pytest.mark.filterwarnings(
@@ -219,11 +221,3 @@ def test_af_warning_from_check_data_size():
         data_dict = {"name1": data}
         with pytest.warns(BadFeatureSizeWarning):
             AncillaryFeature.check_data_size(ds, data_dict)
-
-
-if __name__ == "__main__":
-    # Run all tests
-    loc = locals()
-    for key in list(loc.keys()):
-        if key.startswith("test_") and hasattr(loc[key], "__call__"):
-            loc[key]()

tests/test_rtdc_fmt_hdf5.py
@@ -141,21 +141,29 @@ def test_defective_feature_time_issue_204_new_dclab():
         assert "time" in ds.features_innate  # Recognized as new dclab
 
 
+@pytest.mark.filterwarnings(
+    "ignore::dclab.rtdc_dataset.config.WrongConfigurationTypeWarning")
+@pytest.mark.filterwarnings(
+    'ignore::dclab.rtdc_dataset.config.UnknownConfigurationKeyWarning')
 def test_defective_feature_time_issue_207_with_offset():
     # see https://github.com/DC-analysis/dclab/issues/207
     h5path = retrieve_data("fmt-hdf5_mask-contour_2018.zip")
     # sanity check
     with h5py.File(h5path, "a") as h5:
-        # Write a float32 bit time feature
-        time = np.arange(h5["events/time"].size, dtype=np.float32) + 42
-        assert time[-1] != 42
+        h5.attrs["imaging:frame rate"] = 100
+        # write modified frames
+        frame_w_offset = h5["events/frame"][:]
+        frame_w_offset += 42 - frame_w_offset[0]
+        del h5["events/frame"]
+        h5["events/frame"] = frame_w_offset
+        # write wrong time
+        bad_time = np.arange(h5["events/time"].size, dtype=np.float32)
         del h5["events/time"]
-        h5["events/time"] = time
+        h5["events/time"] = bad_time
 
     with new_dataset(h5path) as ds:
         assert "time" not in ds.features_innate  # Recognized as float32
-        assert ds["time"][0] == 42
-        assert ds["time"][-1] != time[-1]  # because it's computed anew
+        assert np.allclose(ds["time"][0], 0.42, atol=0, rtol=1e-7)
 
 
 @pytest.mark.filterwarnings(
b0e4788c7d1a797fb99d8269dc4dff4ec8915529
Paul Müller <dev@craban.de>
2023-02-08 08:30:38 +0100

    tests: add tests for new buyukurganci-2022 viscosity model (#197)

--- tests/test_feat_emodulus_viscosity.py
+++ tests/test_feat_emodulus_viscosity.py
@@ -1,7 +1,94 @@
 import numpy as np
 import warnings
 
+import pytest
+
 from dclab.features import emodulus
+
+
+def test_buyukurganci_cell_carrier():
+    """Test values inspired from the table in Herold's paper"""
+    ch_sizes = [15, 15, 15, 20, 20, 20, 20, 20, 30, 30, 30, 40, 40, 40]
+    fl_rates = [0.016, 0.032, 0.048, 0.02, 0.04, 0.06,
+                0.08, 0.12, 0.16, 0.24, 0.32, 0.32, 0.40, 0.60]
+    temps = [24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24]
+    eta_a = [
+        5.4001970145102955,
+        4.257647591503594,
+        3.704914977001318,
+        6.725414611510781,
+        5.3024816031010324,
+        4.6141074582608255,
+        4.180606367836839,
+        3.63787533192147,
+        5.0023466326283845,
+        4.352936348316222,
+        3.94397260600829,
+        5.3024816031010324,
+        4.9118302388920805,
+        4.274170894950141,
+    ]
+    eta_b = [
+        6.384624807560792,
+        4.799374333891269,
+        4.061421656375954,
+        8.309306638844017,
+        6.246173301784982,
+        5.285760549704508,
+        4.695299212276897,
+        3.9733491445425506,
+        5.824144616254946,
+        4.928623072237973,
+        4.378056820977706,
+        6.246173301784982,
+        5.697847203911832,
+        4.821745173172601,
+    ]
+
+    for c, f, t, a in zip(ch_sizes, fl_rates, temps, eta_a):
+        eta = emodulus.viscosity.get_viscosity(medium="0.49% MC-PBS",
+                                               channel_width=c,
+                                               flow_rate=f,
+                                               temperature=t,
+                                               model='buyukurganci-2022')
+        assert np.allclose(eta, a)
+
+    for c, f, t, b in zip(ch_sizes, fl_rates, temps, eta_b):
+        eta = emodulus.viscosity.get_viscosity(medium="0.59% MC-PBS",
+                                               channel_width=c,
+                                               flow_rate=f,
+                                               temperature=t,
+                                               model='buyukurganci-2022')
+        assert np.allclose(eta, b)
+
+
+@pytest.mark.parametrize("medium", ["0.49% MC-PBS", "0.59% MC-PBS"])
+def test_buyukurganci_mcpbs_range(medium):
+    # test values
+    with warnings.catch_warnings(record=True) as w:
+        # Cause all warnings to always be triggered.
+        warnings.simplefilter("always")
+        emodulus.viscosity.get_viscosity(
+            medium=medium, temperature=21, model="buyukurganci-2022")
+        assert issubclass(w[-1].category,
+                          emodulus.viscosity.TemperatureOutOfRangeWarning)
+
+    with warnings.catch_warnings(record=True) as w:
+        # Cause all warnings to always be triggered.
+        warnings.simplefilter("always")
+        emodulus.viscosity.get_viscosity(
+            medium=medium, temperature=38, model="buyukurganci-2022")
+        assert issubclass(w[-1].category,
+                          emodulus.viscosity.TemperatureOutOfRangeWarning)
+
+    with warnings.catch_warnings(record=True) as w:
+        # Cause all warnings to always be triggered.
+        warnings.simplefilter("always")
+        emodulus.viscosity.get_viscosity(medium=medium,
+                                         temperature=np.linspace(1, 10, 8),
+                                         model="buyukurganci-2022")
+        assert issubclass(w[-1].category,
+                          emodulus.viscosity.TemperatureOutOfRangeWarning)
 
 
 def test_herold_cell_carrier():
@@ -83,7 +170,7 @@ def test_herold_cellcarrierb_range():
 
 
 def test_kestin_water():
-    """Test with data from Kestin et al, J. Phys. Chem. 7(3) 1978"""
+    """Test with data from Kestin et al., J. Phys. Chem. 7(3) 1978"""
     ref = np.array([[0, 1791.5],
                     [5, 1519.3],
                     [10, 1307.0],

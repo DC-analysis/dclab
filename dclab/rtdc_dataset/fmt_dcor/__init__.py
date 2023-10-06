3969024cb8aac8d68c54ad98e6d37938f97b6d4b
Paul Müller <dev@craban.de>
2023-10-06 15:09:58 +0200

    tests: fix import

--- dclab/rtdc_dataset/fmt_dcor/__init__.py
+++ dclab/rtdc_dataset/fmt_dcor/__init__.py
@@ -2,5 +2,5 @@
 """DCOR client interface"""
 from .api import REQUESTS_AVAILABLE
 from .base import (
-    DCOR_CERTS_SEARCH_PATHS, RTDC_DCOR, is_dcor_url
+    DCOR_CERTS_SEARCH_PATHS, RTDC_DCOR, get_server_cert_path, is_dcor_url
 )

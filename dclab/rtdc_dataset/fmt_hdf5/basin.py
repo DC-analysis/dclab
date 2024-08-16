6a96d1a41b317f4f4426864b7f8d9690154567fd
Paul Müller <dev@craban.de>
2024-08-16 18:21:54 +0200

    enh: improve availability check for HDF5 basin data

--- dclab/rtdc_dataset/fmt_hdf5/basin.py
+++ dclab/rtdc_dataset/fmt_hdf5/basin.py
@@ -12,14 +12,18 @@ class HDF5Basin(feat_basin.Basin):
     basin_format = "hdf5"
     basin_type = "file"
 
+    def __init__(self, *args, **kwargs):
+        self._available_verified = None
+        super(HDF5Basin, self).__init__(*args, **kwargs)
+
     def _load_dataset(self, location, **kwargs):
         return RTDC_HDF5(location, **kwargs)
 
     def is_available(self):
-        with self._av_check_lock:
-            avail = False
-            try:
-                avail = pathlib.Path(self.location).exists()
-            except OSError:
-                pass
-        return avail
+        if self._available_verified is None:
+            with self._av_check_lock:
+                try:
+                    self._available_verified = pathlib.Path(self.location).exists()
+                except OSError:
+                    pass
+        return self._available_verified

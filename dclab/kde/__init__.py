71660743f6ba02ff03bdb72f12629600f06d02f1
RaghavaAlajangi <raghava.alajangi95@gmail.com>
2025-04-02 15:59:37 +0200

    remove incorrect deprecation warnings

--- dclab/kde/__init__.py
+++ dclab/kde/__init__.py
@@ -1,10 +1,1 @@
-# flake8: noqa: F401
-import warnings
-
-from .base import KernelDensityEstimator
-
-warnings.warn(
-    "Modules have been moved to 'kde'. Update your imports accordingly.",
-    DeprecationWarning,
-    stacklevel=2
-)
+from .base import KernelDensityEstimator  # noqa: F401

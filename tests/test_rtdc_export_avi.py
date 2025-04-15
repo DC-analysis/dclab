4304af625983339a2c19224dcb4befe41339ec44
Paul Müller <dev@craban.de>
2025-04-16 00:12:01 +0200

    tests: minor cleanup

--- tests/test_rtdc_export_avi.py
+++ tests/test_rtdc_export_avi.py
@@ -1,7 +1,3 @@
-import os
-from os.path import join
-import tempfile
-
 import pytest
 
 import dclab
@@ -34,44 +30,40 @@ def test_avi_export_check(tmp_path):
     "ignore::dclab.rtdc_dataset.config.WrongConfigurationTypeWarning")
 @pytest.mark.filterwarnings('ignore::dclab.rtdc_dataset.'
                             + 'fmt_tdms.exc.CorruptFrameWarning')
-def test_avi_export_tdms():
+def test_avi_export_tdms(tmp_path):
     pytest.importorskip("nptdms")
     ds = new_dataset(retrieve_data("fmt-tdms_fl-image_2016.zip"))
-    edest = tempfile.mkdtemp()
-    f1 = join(edest, "test.avi")
+
+    f1 = tmp_path / "test.avi"
     ds.export.avi(path=f1)
-    assert os.stat(
-        f1)[6] > 1e4, "Resulting file to small, Something went wrong!"
+    assert f1.stat()[6] > 1e4, "Resulting file to small, Something went wrong!"
 
 
 @pytest.mark.filterwarnings(
     "ignore::dclab.rtdc_dataset.config.WrongConfigurationTypeWarning")
 @pytest.mark.filterwarnings('ignore::dclab.rtdc_dataset.'
                             + 'fmt_tdms.exc.CorruptFrameWarning')
-def test_avi_override_tdms():
+def test_avi_override_tdms(tmp_path):
     pytest.importorskip("nptdms")
     ds = new_dataset(retrieve_data("fmt-tdms_fl-image_2016.zip"))
 
-    edest = tempfile.mkdtemp()
-    f1 = join(edest, "test.avi")
+    f1 = tmp_path / "test.avi"
     ds.export.avi(f1, override=True)
     try:
-        ds.export.avi(f1[:-4], override=False)
+        ds.export.avi(f1.with_name(f1.stem), override=False)
     except OSError:
         pass
     else:
         raise ValueError("Should append .avi and not override!")
 
 
-def test_avi_no_images():
+def test_avi_no_images(tmp_path):
     keys = ["area_um", "deform", "time", "frame", "fl3_width"]
     ddict = example_data_dict(size=127, keys=keys)
     ds = dclab.new_dataset(ddict)
 
-    edest = tempfile.mkdtemp()
-    f1 = join(edest, "test.avi")
     try:
-        ds.export.avi(f1)
+        ds.export.avi(tmp_path / "test.avi")
     except OSError:
         pass
     else:

bf73aa5f5588e4dbf2ed24851097872e845e0dfd
RaghavaAlajangi <raghava.alajangi95@gmail.com>
2025-05-14 11:16:02 +0200

    test: add test for get_contour_lines method in KernelDensityEstimator

---
+++ tests/test_kde.py
@@ -0,0 +1,38 @@
+import numpy as np
+import dclab
+from dclab.external import skimage
+from dclab.kde import KernelDensityEstimator
+from dclab import polygon_filter
+
+
+def test_contour_lines():
+    np.random.seed(47)
+    x0 = np.random.normal(loc=100, scale=10, size=100)
+    y0 = np.random.normal(loc=.1, scale=.01, size=100)
+
+    ds = dclab.new_dataset({"area_um": x0, "deform": y0})
+    ds.config["filtering"]["enable filters"] = False
+
+    kde_instance = KernelDensityEstimator(ds)
+
+    contours = kde_instance.get_contour_lines(xax="area_um",
+                                              yax="deform",
+                                              xacc=.10,
+                                              yacc=.01,
+                                              kde_type="histogram",
+                                              kde_kwargs=None,
+                                              xscale="linear",
+                                              yscale="linear",
+                                              quantiles=[0.89])
+    nump = 0
+    for p in zip(x0, y0):
+        nump += polygon_filter.PolygonFilter.point_in_poly(p,
+                                                           poly=contours[0][0])
+
+    assert nump == 11, "there should be (1-q)*100 points in the contour"
+
+    # added in dclab 0.24.1
+    nump2 = skimage.measure.points_in_poly(
+        np.concatenate((x0.reshape(-1, 1), y0.reshape(-1, 1)), axis=1),
+        contours[0][0])
+    assert nump2.sum() == 11

1262a14d5f4efd8d0a7bbcdf506a269a94a31f6d
Paul Müller <dev@craban.de>
2025-05-14 16:41:32 +0200

    setup: drop support for Python 3.8

--- dclab/rtdc_dataset/feat_basin.py
+++ dclab/rtdc_dataset/feat_basin.py
@@ -64,7 +64,7 @@ class PerishableRecord:
     available again).
     """
     def __init__(self,
-                 basin,  # TODO: add type hint once we drop Python 3.8
+                 basin,
                  expiration_func: Callable = None,
                  expiration_kwargs: Dict = None,
                  refresh_func: Callable = None,
@@ -185,7 +185,7 @@ class Basin(abc.ABC):
                  mapping_referrer: Dict = None,
                  ignored_basins: List[str] = None,
                  key: str = None,
-                 perishable=False,  # TODO: type hint once we drop Python 3.8
+                 perishable=False,
                  **kwargs):
         """
 

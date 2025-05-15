1a3f0b500f7302565e8a386d4c23cc0fb726bac3
RaghavaAlajangi <raghava.alajangi95@gmail.com>
2025-05-15 17:21:51 +0200

    test: add test for contour lines with excessive spacing

--- tests/test_kde.py
+++ tests/test_kde.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pytest
 import dclab
 from dclab.external import skimage
 from dclab.kde import KernelDensityEstimator
@@ -71,3 +72,30 @@ def test_contour_lines_without_quantiles_with_ret_levels():
     assert nump2.sum() == 50
 
     assert len(levels) == 2, "there should be two levels"
+
+
+@pytest.mark.filterwarnings("ignore::UserWarning", "ignore::RuntimeWarning")
+def test_contour_lines_data_with_too_much_space():
+    np.random.seed(47)
+    x0 = np.random.normal(loc=100, scale=10, size=100)
+    y0 = np.random.normal(loc=.1, scale=.01, size=100)
+
+    n = 100  # Adjust this to control spacing
+    x0 = x0[::n]
+    y0 = y0[::n]
+
+    ds = dclab.new_dataset({"area_um": x0, "deform": y0})
+    ds.config["filtering"]["enable filters"] = False
+
+    kde_instance = KernelDensityEstimator(ds)
+
+    contours = kde_instance.get_contour_lines(xax="area_um",
+                                              yax="deform",
+                                              xacc=.10,
+                                              yacc=.01,
+                                              kde_type="histogram",
+                                              kde_kwargs=None,
+                                              xscale="linear",
+                                              yscale="linear",
+                                              quantiles=[0.5])
+    assert not contours, "there should be no contours with too much space"

871d22323bf0b408d258a42ac631ab178ee74b6c
Paul Müller <dev@craban.de>
2026-03-26 08:54:05 +0100

    pep8

--- dclab/cached/disk_store.py
+++ dclab/cached/disk_store.py
@@ -186,15 +186,15 @@ class DiskStore:
 
 class DiskStoreJSONEncoder(json.JSONEncoder):
     """Custom JSONEncoder"""
-    def default(self, obj):
-        if isinstance(obj, pathlib.Path):
-            return str(obj)
-        elif isinstance(obj, numbers.Integral):
-            return int(obj)
-        elif isinstance(obj, np.bool_):
-            return bool(obj)
+    def default(self, o):
+        if isinstance(o, pathlib.Path):
+            return str(o)
+        elif isinstance(o, numbers.Integral):
+            return int(o)
+        elif isinstance(o, np.bool_):
+            return bool(o)
         # Let the base class default method raise the TypeError
-        return json.JSONEncoder.default(self, obj)
+        return json.JSONEncoder.default(self, o)
 
 
 class LockFile:
